ef068368bd4500bfa5d4d2cf06be2773e3d24241
Giulio Benetti <giulio.benetti@micronovasrl.com>
2017-11-02 16:44:36 +0100

    add makefile based on legacy r3px userspace drivers

---
+++ Makefile
@@ -0,0 +1,26 @@
+.PHONY : test
+
+all: config.mk
+	$(MAKE) -C include
+	$(MAKE) -C lib
+
+config:
+	rm -f config.mk
+	$(MAKE) config.mk
+
+config.mk:
+	$(MAKE) -f Makefile.config
+
+clean:
+	$(MAKE) -C version clean
+	$(MAKE) -C test clean
+	$(MAKE) -C lib clean
+	$(MAKE) -C include clean
+	rm -f config.mk
+
+install: config.mk
+	$(MAKE) -C lib install
+	$(MAKE) -C include install
+
+test: config.mk
+	$(MAKE) -C test test
---
+++ test/test.c
@@ -0,0 +1,329 @@
+/*
+ * Copyright (c) 2011-2013 Luc Verhaegen <libv@skynet.be>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sub license,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Hello triangle, adapted for native display on libMali.so.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+#define WIDTH 480
+#define HEIGHT 480
+
+#ifdef _X11_XLIB_H_
+Display *XDisplay;
+Window XWindow;
+#else
+struct fbdev_window native_window = {
+	.width = WIDTH,
+	.height = HEIGHT,
+};
+#endif
+
+static const char *vertex_shader_source =
+	"attribute vec4 aPosition;    \n"
+	"attribute vec4 aColor;       \n"
+	"                             \n"
+	"varying vec4 vColor;         \n"
+	"                             \n"
+	"void main()                  \n"
+	"{                            \n"
+	"    vColor = aColor;         \n"
+	"    gl_Position = aPosition; \n"
+	"}                            \n";
+static const char *fragment_shader_source =
+	"precision mediump float;     \n"
+	"                             \n"
+	"varying vec4 vColor;         \n"
+	"                             \n"
+	"void main()                  \n"
+	"{                            \n"
+	"    gl_FragColor = vColor;   \n"
+	"}                            \n";
+
+static GLfloat vVertices[] = {  0.0f,  0.5f, 0.0f,
+			       -0.5f, -0.5f, 0.0f,
+				0.5f, -0.5f, 0.0f };
+static GLfloat vColors[] = {1.0f, 0.0f, 0.0f, 1.0f,
+			    0.0f, 1.0f, 0.0f, 1.0f,
+			    0.0f, 0.0f, 1.0f, 1.0f};
+
+static EGLint const config_attribute_list[] = {
+	EGL_RED_SIZE, 8,
+	EGL_GREEN_SIZE, 8,
+	EGL_BLUE_SIZE, 8,
+	EGL_ALPHA_SIZE, 8,
+	EGL_BUFFER_SIZE, 32,
+
+	EGL_STENCIL_SIZE, 0,
+	EGL_DEPTH_SIZE, 0,
+
+	EGL_SAMPLES, 4,
+
+	EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+
+	EGL_SURFACE_TYPE, EGL_WINDOW_BIT | EGL_PIXMAP_BIT,
+
+
+	EGL_NONE
+};
+
+static EGLint window_attribute_list[] = {
+	EGL_NONE
+};
+
+static const EGLint context_attribute_list[] = {
+	EGL_CONTEXT_CLIENT_VERSION, 2,
+	EGL_NONE
+};
+
+EGLDisplay egl_display;
+EGLSurface egl_surface;
+
+void
+Redraw(int width, int height)
+{
+	glViewport(0, 0, width, height);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	eglSwapBuffers(egl_display, egl_surface);
+}
+
+int
+main(int argc, char *argv[])
+{
+	EGLint egl_major, egl_minor;
+	EGLConfig config;
+	EGLint num_config;
+	EGLContext context;
+	GLuint vertex_shader;
+	GLuint fragment_shader;
+	GLuint program;
+	GLint ret;
+	GLint width, height;
+
+#ifdef _X11_XLIB_H_
+	XDisplay = XOpenDisplay(NULL);
+	if (!XDisplay) {
+		fprintf(stderr, "Error: failed to open X display.\n");
+		return -1;
+	}
+
+	Window XRoot = DefaultRootWindow(XDisplay);
+
+	XSetWindowAttributes XWinAttr;
+	XWinAttr.event_mask  =  ExposureMask | PointerMotionMask;
+
+	XWindow = XCreateWindow(XDisplay, XRoot, 0, 0, WIDTH, HEIGHT, 0,
+				CopyFromParent, InputOutput,
+				CopyFromParent, CWEventMask, &XWinAttr);
+
+	Atom XWMDeleteMessage =
+		XInternAtom(XDisplay, "WM_DELETE_WINDOW", False);
+
+	XMapWindow(XDisplay, XWindow);
+	XStoreName(XDisplay, XWindow, "Mali libs test");
+	XSetWMProtocols(XDisplay, XWindow, &XWMDeleteMessage, 1);
+
+	egl_display = eglGetDisplay((EGLNativeDisplayType) XDisplay);
+#else
+	egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+#endif /* _X11_XLIB_H_ */
+	if (egl_display == EGL_NO_DISPLAY) {
+		fprintf(stderr, "Error: No display found!\n");
+		return -1;
+	}
+
+	if (!eglInitialize(egl_display, &egl_major, &egl_minor)) {
+		fprintf(stderr, "Error: eglInitialise failed!\n");
+		return -1;
+	}
+
+	printf("EGL Version: \"%s\"\n",
+	       eglQueryString(egl_display, EGL_VERSION));
+	printf("EGL Vendor: \"%s\"\n",
+	       eglQueryString(egl_display, EGL_VENDOR));
+	printf("EGL Extensions: \"%s\"\n",
+	       eglQueryString(egl_display, EGL_EXTENSIONS));
+
+	eglChooseConfig(egl_display, config_attribute_list, &config, 1,
+			&num_config);
+
+	context = eglCreateContext(egl_display, config, EGL_NO_CONTEXT,
+				   context_attribute_list);
+	if (context == EGL_NO_CONTEXT) {
+		fprintf(stderr, "Error: eglCreateContext failed: 0x%08X\n",
+			eglGetError());
+		return -1;
+	}
+
+#ifdef _X11_XLIB_H_
+	egl_surface = eglCreateWindowSurface(egl_display, config, XWindow,
+					     window_attribute_list);
+#else
+	egl_surface = eglCreateWindowSurface(egl_display, config,
+					     &native_window,
+					     window_attribute_list);
+#endif
+	if (egl_surface == EGL_NO_SURFACE) {
+		fprintf(stderr, "Error: eglCreateWindowSurface failed: "
+			"0x%08X\n", eglGetError());
+		return -1;
+	}
+
+	if (!eglQuerySurface(egl_display, egl_surface, EGL_WIDTH, &width) ||
+	    !eglQuerySurface(egl_display, egl_surface, EGL_HEIGHT, &height)) {
+		fprintf(stderr, "Error: eglQuerySurface failed: 0x%08X\n",
+			eglGetError());
+		return -1;
+	}
+	printf("Surface size: %dx%d\n", width, height);
+
+	if (!eglMakeCurrent(egl_display, egl_surface, egl_surface, context)) {
+		fprintf(stderr, "Error: eglMakeCurrent() failed: 0x%08X\n",
+			eglGetError());
+		return -1;
+	}
+
+	printf("GL Vendor: \"%s\"\n", glGetString(GL_VENDOR));
+	printf("GL Renderer: \"%s\"\n", glGetString(GL_RENDERER));
+	printf("GL Version: \"%s\"\n", glGetString(GL_VERSION));
+	printf("GL Extensions: \"%s\"\n", glGetString(GL_EXTENSIONS));
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	if (!vertex_shader) {
+		fprintf(stderr, "Error: glCreateShader(GL_VERTEX_SHADER) "
+			"failed: 0x%08X\n", glGetError());
+		return -1;
+	}
+
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
+
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &ret);
+	if (!ret) {
+		char *log;
+
+		fprintf(stderr, "Error: vertex shader compilation failed!\n");
+		glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &ret);
+
+		if (ret > 1) {
+			log = malloc(ret);
+			glGetShaderInfoLog(vertex_shader, ret, NULL, log);
+			fprintf(stderr, "%s", log);
+		}
+		return -1;
+	}
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	if (!fragment_shader) {
+		fprintf(stderr, "Error: glCreateShader(GL_FRAGMENT_SHADER) "
+			"failed: 0x%08X\n", glGetError());
+		return -1;
+	}
+
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
+
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &ret);
+	if (!ret) {
+		char *log;
+
+		fprintf(stderr, "Error: fragment shader compilation failed!\n");
+		glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &ret);
+
+		if (ret > 1) {
+			log = malloc(ret);
+			glGetShaderInfoLog(fragment_shader, ret, NULL, log);
+			fprintf(stderr, "%s", log);
+		}
+		return -1;
+	}
+
+	program = glCreateProgram();
+	if (!program) {
+		fprintf(stderr, "Error: failed to create program!\n");
+		return -1;
+	}
+
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+
+	glBindAttribLocation(program, 0, "aPosition");
+	glBindAttribLocation(program, 1, "aColor");
+
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &ret);
+	if (!ret) {
+		char *log;
+
+		fprintf(stderr, "Error: program linking failed!\n");
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &ret);
+
+		if (ret > 1) {
+			log = malloc(ret);
+			glGetProgramInfoLog(program, ret, NULL, log);
+			fprintf(stderr, "%s", log);
+		}
+		return -1;
+	}
+	glUseProgram(program);
+
+	glClearColor(0.2, 0.2, 0.2, 1.0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, vColors);
+	glEnableVertexAttribArray(1);
+
+	Redraw(width, height);
+
+#ifdef _X11_XLIB_H_
+	while (1) {
+		XEvent event;
+
+		XNextEvent(XDisplay, &event);
+
+		if ((event.type == MotionNotify) ||
+		    (event.type == Expose))
+			Redraw(width, height);
+		else if (event.type == ClientMessage) {
+			if (event.xclient.data.l[0] == XWMDeleteMessage)
+				break;
+		}
+	}
+	XSetWMProtocols(XDisplay, XWindow, &XWMDeleteMessage, 0);
+#endif
+
+	return 0;
+}
